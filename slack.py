7714e1937b6ac9e034c65130e70ba694fc07d96c
yuta <katayama@xshell.io>
2018-02-12 15:56:16 +0900

    Update slack.py

--- slack.py
+++ slack.py
@@ -21,8 +21,8 @@ except KeyError as e:
 
 IMAGE_FILE = 'hello.jpg'
 
-net = cv2.dnn.readNetFromCaffe('models/MobileNetSSD_deploy.prototxt.txt',
-        'models/MobileNetSSD_deploy.caffemodel')
+net = cv2.dnn.readNetFromCaffe('/home/pi/MobileNetSSD_deploy.prototxt.txt',
+        '/home/pi/MobileNetSSD_deploy.caffemodel')
 
 
 def upload():
